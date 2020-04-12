aec5a2a4dd313161e9afb97419179510ae6fc07a
okureiman <ranyakumo7@gmail.com>
2020-04-12 06:52:13 +0000

    first commit

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+ 
+@section('title', 'ログイン成功')
+ 
+@section('content')
+ログインできました。
+@endsection
---
+++ resources/views/admin/game/login.blade.php
@@ -0,0 +1,14 @@
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.admin')
+
+@section('title', 'ログイン画面')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>アカウント作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
