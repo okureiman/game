f065909e67fa5994a9d02ccf0b4d22a1fe7f3c32
okureiman <ranyakumo7@gmail.com>
2020-06-30 14:33:02 +0000

    google del

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -59,14 +59,6 @@
                                     {{ __('messages.Login') }}
                                 </button>
                                 
-                                <div class="form-group row mt-2">
-                                   <div class="col-md-8 offset-md-4">
-                                      <a href="/login/google" class="btn btn-secondary" role="button">
-                                        Googleアカウントでログイン
-                                     </a>
-                                   </div>
-                                </div>
-
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('messages.Forgot Your Password?') }}
